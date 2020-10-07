e868335161821d9c469b5d45bfc63bbd4719ab1b
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-07 12:51:17 +0300

    Lectures.Lec01_StacksAndQueues.Ex04_MatchingBrackets

---
+++ src/Lectures/Lec01_StacksAndQueues/Ex04_MatchingBrackets.java
@@ -0,0 +1,30 @@
+package Lectures.Lec01_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Ex04_MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        Deque<Integer> bracketIndex = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '('){
+                bracketIndex.push(i);
+            }
+            if (ch==')'){
+                for (int j = bracketIndex.peek(); j <= i; j++) {
+                    char printChar = expression.charAt(j);
+                    System.out.print(printChar);
+                }
+                bracketIndex.pop();
+                System.out.println();
+            }
+        }
+    }
+}
