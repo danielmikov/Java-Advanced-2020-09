2ca9579eaeb9fd387da0a8e923a7a0077eb78456
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-07 12:17:15 +0300

    Lectures.Lec01_StacksAndQueues.Ex03_DecimalToBinary

---
+++ src/Lectures/Lec01_StacksAndQueues/Ex03_DecimalToBinary.java
@@ -0,0 +1,29 @@
+package Lectures.Lec01_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Ex03_DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        Deque<Integer> binaryNumber = new ArrayDeque<>();
+
+        if (number == 0) {
+            System.out.println(number);
+        } else {
+            while (number > 0) {
+                binaryNumber.push(number % 2);
+                number /= 2;
+            }
+
+            while (binaryNumber.size() > 0) {
+                System.out.print(binaryNumber.pop());
+            }
+        }
+
+    }
+}
