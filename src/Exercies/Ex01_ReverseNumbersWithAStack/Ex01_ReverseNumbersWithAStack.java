f72a5a7f80acc6c5e59ffd2c34b4d6b87f409e7f
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-09 15:21:54 +0300

    Added Exercies.Ex01_ReverseNumbersWithAStack.Ex01_ReverseNumbersWithAStack

---
+++ src/Exercies/Ex01_ReverseNumbersWithAStack/Ex01_ReverseNumbersWithAStack.java
@@ -0,0 +1,22 @@
+package Exercies.Ex01_ReverseNumbersWithAStack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Ex01_ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+        Deque<String> queue = new ArrayDeque<>();
+
+        for (String e : input) {
+            queue.push(e);
+        }
+        for (String e : queue) {
+            System.out.print(queue.pop() + " ");
+        }
+
+    }
+}
