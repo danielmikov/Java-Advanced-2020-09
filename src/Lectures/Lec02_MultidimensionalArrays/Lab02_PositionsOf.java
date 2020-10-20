ed06e410811f04e8268b6b56e666e9e2ed7817da
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-20 15:00:27 +0300

    Add Lectures.Lec02_MultidimensionalArrays.Lab02_PositionsOf

---
+++ src/Lectures/Lec02_MultidimensionalArrays/Lab02_PositionsOf.java
@@ -0,0 +1,36 @@
+package Lectures.Lec02_MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lab02_PositionsOf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int r = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.nextLine();
+
+        int[][] matrix = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            matrix[i] = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        boolean xIsPresent = false;
+        int x = Integer.parseInt(scanner.nextLine()); //find x in Matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int currentNumber = matrix[i][j];
+                if (currentNumber==x){
+                    xIsPresent = true;
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+
+        if (!xIsPresent){
+            System.out.println("not found");
+        }
+
+    }
+}
