b0b9e7ba63b154a5fed108d3b22ce7c12c5d9e1c
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-21 19:05:27 +0300

    Add Exercies.Ex08_SetsAndMapsAdvanced.Ex03_PeriodicTable

---
+++ src/Exercies/Ex08_SetsAndMapsAdvanced/Ex03_PeriodicTable.java
@@ -0,0 +1,24 @@
+package Exercies.Ex08_SetsAndMapsAdvanced;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Ex03_PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nChemicals = Integer.parseInt(scanner.nextLine());
+        TreeSet<String> elements = new TreeSet<>();
+
+        for (int i = 0; i < nChemicals; i++) {
+            String[] chemical = scanner.nextLine().split("\\s+");
+            for (String element : chemical) {
+                elements.add(element);
+            }
+        }
+
+        for (String element : elements) {
+            System.out.print(element + " ");
+        }
+    }
+}
