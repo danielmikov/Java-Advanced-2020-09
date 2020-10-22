bda2c66915c4f025cf859e8cc44a0512bfc427bc
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-22 12:51:04 +0300

    Add: Exercises.Ex10_StreamsFilesAndDirectories.Ex01_SumLines

---
+++ src/Exercises/Ex10_StreamsFilesAndDirectories/Ex01_SumLines.java
@@ -0,0 +1,41 @@
+package Exercises.Ex10_StreamsFilesAndDirectories;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Ex01_SumLines {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Path.of("H:\\Documents\\SoftUni\\Java Advanced - 2020-09\\src\\Exercises\\Ex10_StreamsFilesAndDirectories\\Resources\\Files_And_Streams\\input.txt");
+
+        List<String> lines = Files.readAllLines(path);
+
+        lines.stream().map(String::chars);
+
+        int sum = 0;
+
+        for (int line = 0; line < lines.size(); line++) {
+            for (int word = 0; word < lines.get(line).length(); word++) {
+                sum += lines.get(line).charAt(word);
+            }
+            System.out.println(sum);
+            sum = 0;
+        }
+/*
+        Their solution:
+
+        List<String> lines1 = Files.readAllLines(path);
+        lines1.stream().map(String::toCharArray).forEach(charArray ->
+                {
+                    int sum1 = 0;
+                    for (char symbol : charArray) {
+                        sum1 += symbol;
+                    }
+                    System.out.println(sum1);
+                }
+        );
+*/
+    }
+}
