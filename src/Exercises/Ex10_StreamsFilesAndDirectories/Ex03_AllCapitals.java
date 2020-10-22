693782e64ebf02451e2d6ef463b32c7a6cff92de
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-22 13:34:04 +0300

    Add: Exercises.Ex10_StreamsFilesAndDirectories.Ex03_AllCapitals

---
+++ src/Exercises/Ex10_StreamsFilesAndDirectories/Ex03_AllCapitals.java
@@ -0,0 +1,19 @@
+package Exercises.Ex10_StreamsFilesAndDirectories;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Ex03_AllCapitals {
+    public static void main(String[] args) throws IOException {
+        Path pathIn = Path.of("H:\\Documents\\SoftUni\\Java Advanced - 2020-09\\src\\Exercises\\Ex10_StreamsFilesAndDirectories\\Resources\\Files_And_Streams\\input.txt");
+
+        List<String> lines = Files.readAllLines(pathIn);
+
+        PrintWriter printWriter = new PrintWriter("H:\\Documents\\SoftUni\\Java Advanced - 2020-09\\src\\Exercises\\Ex10_StreamsFilesAndDirectories\\Resources\\Files_And_Streams\\output.txt");
+        lines.forEach( line -> printWriter.println(line.toUpperCase()));
+        printWriter.close();
+    }
+}
